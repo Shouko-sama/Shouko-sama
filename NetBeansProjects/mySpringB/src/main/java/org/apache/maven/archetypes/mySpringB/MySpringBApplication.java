5923f03e3b4a56776ab2d71132dccaacb2ba144d
Shouko-sama <mbosupi@gmail.com>
2024-10-18 12:58:26 +0200

    Add files via upload

---
+++ NetBeansProjects/mySpringB/src/main/java/org/apache/maven/archetypes/mySpringB/MySpringBApplication.java
@@ -0,0 +1,13 @@
+package org.apache.maven.archetypes.mySpringB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBApplication.class, args);
+	}
+
+}
